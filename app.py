6efa1935ae647fea94062a20278f802eaf805461
unknown <rayanecsantosm@gmail.com>
2024-03-07 12:00:08 -0400

    envio de variaveis pelo endpoint

--- app.py
+++ app.py
@@ -10,5 +10,20 @@ def index():
 def rotadois(): 
     return "ola"
 
+@app.route("/rayane/<variavel>", methods = ["GET"])
+def rayane(variavel):    
+    return "valor da variavel: " +variavel
+
+@app.route("/santos/<variavel>/<var2>/<var3>", methods = ["GET"])
+def santos(variavel, var2, var3):
+    juntar = variavel+" "+var2+" "+var3    
+    return "valor da variavel: " +juntar
+
+@app.route("/list-user/<int:id>", methods = ["GET"])
+def list_user(id):    
+    return "id do usuario: " +str(id)
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
+
