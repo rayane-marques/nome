2faa8ab595a184955eade0bbbc4c2cdb12c1ad2d
unknown <rayanecsantosm@gmail.com>
2024-03-08 11:56:46 -0400

    envio de variaveis pelo endpoint parte 2

--- app.py
+++ app.py
@@ -1,27 +1,71 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from model.db import MODEL
 
 app = Flask(__name__);
+CORS(app)
 
 @app.route("/rota", methods = ["POST"])
 def index(): 
     return "oi"
 
+
 @app.route("/rotadois", methods = ["GET"])
 def rotadois(): 
     return "ola"
 
+
 @app.route("/rayane/<variavel>", methods = ["GET"])
 def rayane(variavel):    
     return "valor da variavel: " +variavel
+
 
 @app.route("/santos/<variavel>/<var2>/<var3>", methods = ["GET"])
 def santos(variavel, var2, var3):
     juntar = variavel+" "+var2+" "+var3    
     return "valor da variavel: " +juntar
 
+
 @app.route("/list-user/<int:id>", methods = ["GET"])
 def list_user(id):    
     return "id do usuario: " +str(id)
+
+
+@app.route("/list/", methods = ["GET"])
+def list():    
+    return MODEL
+
+
+@app.route("/list/name", methods = ["GET"])
+def list_names():    
+    list_names = []
+    for names in MODEL:
+        print(names['nome'])
+        list_names.append(names['nome'])
+    return list_names
+
+
+@app.route("/test-request/", methods = ["GET","POST","DELETE","PUT"])
+def test_request():
+    if request.method == 'GET':
+        print('method==GET')
+    elif request.method == 'POST':
+        print('method == POST')
+    elif request.methos == 'DELETE':
+        print('method == DELETE')
+    else:
+        print('method == PUT')
+    return "a"
+
+
+@app.route("/get-list/", methods = ["POST"])
+def get_list():
+    list = request.get_json()
+    print(list)
+    return 'a'
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ model/db.py
@@ -0,0 +1,29 @@
+MODEL = [ {
+    "nome": "rayane",
+    "id": "10",
+    "birthday": "29/11/2002"
+},
+
+{
+    "nome": "joao",
+    "id": "1",
+    "birthday": "10/11/2005"
+},
+
+{
+    "nome": "maria",
+    "id": "2",
+    "birthday": "25/03/2000"
+},
+
+{
+    "nome": "marcos",
+    "id": "3",
+    "birthday": "10/11/2005"
+},
+
+{
+    "nome": "peter",
+    "id": "4",
+    "birthday": "14/11/2005"
+} ]
